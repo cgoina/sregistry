5abcc13cdd7db6e3c03bf870b4b9cae04eb3f4ed
Cristian Goina <goinac@janelia.hhmi.org>
2018-05-22 13:05:06 -0400

    extracted the db initialization code from run_uwsgi

---
+++ init_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python manage.py makemigrations
+python manage.py migrate auth
+python manage.py makemigrations users
+python manage.py makemigrations main
+python manage.py makemigrations api
+python manage.py makemigrations logs
+python manage.py migrate
+
